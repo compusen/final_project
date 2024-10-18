360d347c88d142e32e28d2f06358739bdbecf889
compusen <compusen1@hotmail.com>
2024-10-17 23:35:51 -0400

    Updated the file to include the json library

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,15 +1,22 @@
 import requests  # Import the requests library to handle HTTP requests
-
-
+import json  # Import the json library
 # Define a function named emotion_detector that takes a string input (text_to_analyse)
 def emotion_detector(text_to_analyse):
     # URL of the emotion detection service
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-    # Create a dictionary with the text to be analyzed
-    myobj = {"raw_document": {"text": text_to_analyse}}
-    # Set the headers required for the API request
+    myobj = {"raw_document": {"text": text_to_analyse}} # Constructing the request payload in the expected format
     header = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    # Send a POST request to the API with the text and headers
-    response = requests.post(url, json=myobj, headers=header)
-    return response.text  # Return the response text from the API
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }     # Set the headers required for the API request
+    response = requests.post(url, json=myobj, headers=header) # Send a POST request to the emotion detection API with the text and headers
+    formatted_response = json.loads(response.text) # Parsing the JSON response from the API
+    emotions = {
+        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
+        'disgust': formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
+        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
+        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness'],
+    }  # Extracting emotion scores from the response
+    dominant_emotion = max(emotions, key=emotions.get) # Finding the dominant emotion based on the highest score
+    # Returning a dictionary containing emotion detection results
+    return {**emotions, 'dominant_emotion': dominant_emotion}
